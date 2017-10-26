6148f377bc0fe16dadc0ffe918cac19f7ef4a754
Anton Baluev <baluev@outlook.com>
2017-10-26 18:03:56 +0100

    PtaExtensions time superposition stub

--- src/Team76.PTA.Tests/Models/PtaModelTests.cs
+++ src/Team76.PTA.Tests/Models/PtaModelTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using NUnit.Framework;
 using Team76.PTA.Models;
@@ -23,6 +24,38 @@ namespace Team76.PTA.Tests.Models
                 Console.WriteLine($"{time} : {p}");
             }
 
+        }
+
+
+        [Test]
+        public void PtaExtensionsPressureTest()
+        {
+            var fluid = new Fluid() { B = 1, Mu = 1 };
+            var well = new Well() { C = 1, Rw = 0.15, SkinFactor = 0 };
+            var reservoir = new Reservoir() { Ct = 0.00001, Porosity = 0.2, H = 10, K = 10, Pi = 2000 };
+            var pta = new PtaModel() { Fluid = fluid, Well = well, Reservoir = reservoir };
+
+            var times = new List<double>() {0, 5};
+            var rates = new List<double>() {500, 0};
+            
+            
+            var steps = Enumerable.Range(0, 10);
+            foreach (var step in steps)
+            {
+                var p = pta.Pressure(step, times, rates);
+                Console.WriteLine($"{step} : {p}");
+            }
+
+        }
+
+
+    }
+
+    public static class PtaExtensions
+    {
+        public  static double Pressure(this PtaModel pta, double time, List<double> times, List<double> rates)
+        {
+            throw new NotImplementedException();
         }
     }
 }
